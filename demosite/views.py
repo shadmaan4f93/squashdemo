f41603b2051700014a7a39b58f3f0f17a882d397
shadmaan <mshadmaan.dev@gmail.com>
2019-12-05 21:59:28 +0530

    Update message

--- demosite/views.py
+++ demosite/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+import datetime
 
 def index(request):
-    return HttpResponse("My squash deployment demo is working!")
+    now = datetime.datetime.now()
+    html = "<html><body> <div><img src='https://www.dropbox.com/s/g9qb5ai22hr6f72/happy.png?raw=1' width='200' height='200'><br><h1>Hi User</h1> <h4>It is now %s.</h4></div></body></html>" % now
+    return HttpResponse(html)
